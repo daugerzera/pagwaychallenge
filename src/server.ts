b594873df78784aa571a1cd108e4a446488cff7d
Itacir Gabral <itacirgabral@gmail.com>
2023-12-14 16:56:03 -0400

    base express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
-setInterval(() => {
-  console.log('opa')
-}, 1_000)
+import app from './app'
+
+const port = process.env.PORT || '8080'
+
+app.listen(port, () => {
+  console.log(`running at port ${port}`)
+})
