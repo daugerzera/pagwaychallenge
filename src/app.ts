a4fa3f88f8a9917cf21bbe06c1da6c15b62b8f88
Itacir Gabral <itacirgabral@gmail.com>
2023-12-14 17:36:53 -0400

    mock response

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,42 @@ const app = express()
 app.set('trust proxy', 1)
 app.use(cors())
 
+app.post('/cuiabashoes/transacao', express.json(), async (req, res) => {
+  const {
+    valor,
+    descricao,
+    nomePortadorCartao,
+    numeroCartao,
+    validadeCartao,
+    codigoSegurancaCartao
+  } = req.body
+
+  res.status(200).json({
+    ok: true
+  })
+})
+
+app.get('/cuiabashoes/transacao', async (req, res) => {
+  const { page = '1', size = '20', order = 'asc' } = req.query
+
+  res.status(200).json([{
+    "valor": 123,
+    "descricao": "Smartband XYZ 3.0",
+    "nomePortadorCartao": "Amaral B. Cuiabano",
+    "numeroCartao": "1234123412341234",
+    "validadeCartao": "2038/12/31 23:59:59",
+    "codigoSegurancaCartao": "123"
+}])
+})
+
+app.get('/cuiabashoes/saldo', async (req, res) => {
+  res.status(200).json({
+    "saldo": {
+      "disponivel": 0,
+      "previsto": 4200
+    }
+  })
+})
+
+
 export default app
