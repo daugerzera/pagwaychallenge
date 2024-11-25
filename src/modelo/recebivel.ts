7e9da4e01cf313b383562d4cf0f3be6205806f8c
daugerzera <49286169+daugerzera@users.noreply.github.com>
2024-11-25 16:58:38 -0300

    modelos

---
+++ src/modelo/recebivel.ts
@@ -0,0 +1,40 @@
+import sequelize from "../config/db";
+import Sequelize from "sequelize";
+
+const Recebivel = sequelize.define('Recebivel', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    transacao_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+    status_recebivel: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    data_pagamento_recebivel: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    valor_liquido_recebivel: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    tableName: 'recebivel',
+    schema: 'pagway',
+    timestamps: false,
+    indexes: [
+        {
+            name: 'recebivel_status_recebivel_idx',
+            fields: ['status_recebivel'],
+        },
+    ],
+});
+
+module.exports = Recebivel;
