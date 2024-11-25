e71abd99d8313417d66bf1cc0a7c9a0b58b00294
daugerzera <49286169+daugerzera@users.noreply.github.com>
2024-11-25 17:00:59 -0300

    sequelize e modelos

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+if (!process.env.DB_CONN) {
+    throw new Error('A variável de ambiente DB_CONN não foi definida.');
+}
+
+const sequelize = new Sequelize(process.env.DB_CONN, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+export default sequelize;
