aed0833d2b63e7024dbb985958335c27dce5b41d
daugerzera <49286169+daugerzera@users.noreply.github.com>
2024-11-25 17:24:51 -0300

    ajuste app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors'
+import transacaoRotas from './rota/transacoes';
+import saldoRotas from './rota/saldo';
 const Recebivel = require('./models/recebivel');
 const Transacao = require('./models/transacao');
 
 const app = express();
-
+app.set('trust proxy', 1);
+app.use(cors());
 app.use(express.json());
-// app.use('/cashin', cashInRoutes);
+
+app.use('/cuiabashoes', transacaoRotas);
+app.use('/cuiabashoes', saldoRotas);
 
 // Recebivel.belongsTo(Transacao, {
 //     foreignKey: 'transacao_id',
@@ -18,4 +24,13 @@ app.use(express.json());
 //     foreignKey: 'transacao_id',
 // });
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
+
+
+
 export default app;
+
+
